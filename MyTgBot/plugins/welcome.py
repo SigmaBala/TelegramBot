3e4ce326261b30b94a609e71c4e73f741514a918
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-04-18 19:27:45 +0530

    Update welcome.py

--- MyTgBot/plugins/welcome.py
+++ MyTgBot/plugins/welcome.py
@@ -6,9 +6,8 @@ WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
-        name = m.new_chat_members[0].mention if m.new_chat_members else m.from_user.mention
-        await m.reply_animation(WEL_GIF, caption="Hello dear {}\nWelcome to {}!".format(name, m.chat.title))
+        await m.reply_animation(WEL_GIF,caption="Hello dear {}\nWelcome to **{}**!".format(m.from_user.mention,m.chat.title))
         
 @bot.on_message(filters.left_chat_member)
 async def member_has_left(_, m: Message):
-        await m.reply("Sad to see you leaving {}\nTake Care!".format(m.from_user.mention))
+        await m.reply("Sad to see you leaving **{}**\nTake Care!".format(m.from_user.mention))
