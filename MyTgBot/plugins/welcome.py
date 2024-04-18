4f5fb6b61c01def1dc82ad72d64d2b685dee5202
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-04-18 19:22:46 +0530

    Update welcome.py

--- MyTgBot/plugins/welcome.py
+++ MyTgBot/plugins/welcome.py
@@ -6,7 +6,8 @@ WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
-        await m.reply(m)
+        name = m.new_chat_members[0].mention if m.new_chat_members else m.from_user.mention
+        await m.reply_animation(WEL_GIF, caption="Hello dear {}\nWelcome to {}!".format(name, m.chat.title))
         
 @bot.on_message(filters.left_chat_member)
 async def member_has_left(_, m: Message):
