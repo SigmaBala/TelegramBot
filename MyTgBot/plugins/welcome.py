f5bb5c3b35ac34a2d05f851a55b3440a558aae2b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-18 16:49:32 +0530

    Update welcome.py

--- MyTgBot/plugins/welcome.py
+++ MyTgBot/plugins/welcome.py
@@ -6,8 +6,7 @@ WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
-        name = m.new_chat_members[0].mention if m.new_chat_members else m.from_user.mention
-        await m.reply_animation(WEL_GIF, caption="Hello dear {}\nWelcome to {}!".format(name, m.chat.title))
+        await m.reply(m)
         
 @bot.on_message(filters.left_chat_member)
 async def member_has_left(_, m: Message):
