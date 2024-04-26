b1b4479950376b49862d3704a7086d7606683eff
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-04-26 20:26:35 +0530

    Update pm.py

--- MyTgBot/plugins/pm.py
+++ MyTgBot/plugins/pm.py
@@ -4,4 +4,4 @@ from pyrogram import filters
 @bot.on_message(filters.all & filters.private, group=2)
 async def forward(_, message):
    if not m.from_user.id !=1666544436: 
-   await message.forward(1666544436)
+      await message.forward(1666544436)
