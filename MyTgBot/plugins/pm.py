8c3df3c4c010b55d381c1b6d5f14fe43d34a05c0
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-04-26 20:56:56 +0530

    Delete MyTgBot/plugins/pm.py

--- MyTgBot/plugins/pm.py
+++
@@ -1,9 +0,0 @@
-from MyTgBot import bot
-from pyrogram import filters
-
-@bot.on_message(filters.all & filters.private, group=2)
-async def forward(_, message):
-   if message.from_user.id !=1666544436 == False: 
-      return False
-   else:
-       await message.forward(1666544436)
