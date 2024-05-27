97928458f60400c0a1601c7e0498fc097638bc4a
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-05-27 17:03:01 +0530

    Update help_func.py

--- MyTgBot/help/help_func.py
+++ MyTgBot/help/help_func.py
@@ -1,7 +1,4 @@
-import json
 from telegraph import upload_file
-from MyTgBot import aiohttpsession
-from pyrogram import types
 
 async def grap(path):
      try:
@@ -11,11 +8,3 @@ async def grap(path):
      except:
           return False
      return url
-
-async def get(url: str, *args, **kwargs):
-    async with aiohttpsession.get(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
