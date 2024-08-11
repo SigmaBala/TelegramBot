41eab8b7969197e825234980872334774ad6cfb8
Naveen -TG,🇮🇳 [#offline] <93958628+Naveen-TG@users.noreply.github.com>
2024-08-11 11:56:52 +0530

    Update __init__.py

--- MyTgBot/__init__.py
+++ MyTgBot/__init__.py
@@ -20,3 +20,12 @@ bot = Client("MyTgBot",
        api_hash=api_hash,
        bot_token=bot_token,
        plugins=dict(root="MyTgBot"), )
+
+# koyeb
+
+PORT = "8080"
+
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
