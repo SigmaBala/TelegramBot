923822d345702a0fb8954a376f74b3426c93bd90
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-11-12 08:07:39 +0530

    Update mute.py

--- MyTgBot/plugins/mute.py
+++ MyTgBot/plugins/mute.py
@@ -14,7 +14,8 @@ async def muted(_, m):
       api = requests.get("https://nekos.best/api/v2/bored").json()
       url = api["results"][0]['url']
       try:
-          if admin.privileges.can_restrict_members:   
+          if admin.privileges.can_restrict_members:
+              return await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
                 if not reply and len(m.command) >2:
                     mute_id = int(m.text.split(" ")[1])
                     reason = m.text.split(None, 2)[2]
