096f7a048eb196b4ef095a3cae2e1c4b6df948c4
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-11-27 19:38:19 +0530

    Update mute.py

--- MyTgBot/plugins/mute.py
+++ MyTgBot/plugins/mute.py
@@ -32,7 +32,7 @@ async def muted(_, m):
                 else:
                      await bot.restrict_chat_member(chat_id, mute_id, ChatPermissions(can_send_messages=False))
                      await m.reply_text(f"The Bitch Muted!\n • {reply.from_user.mention}\n\nFollowing Reason:\n`{reason}`")
-   except Exception as e:
+ except Exception as e:
          await m.reply_text(e)
 
 
