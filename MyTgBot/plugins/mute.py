4df48f1a138ef36367e84b4eee2df8dee878486f
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-11-27 18:49:16 +0530

    Update mute.py

--- MyTgBot/plugins/mute.py
+++ MyTgBot/plugins/mute.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 
 @bot.on_message(filters.command("mute",  ["/", ".", "?", "!"]))
 async def muted(_, m):
-    if len(message.text.split()) > 1:
+      if len(message.text.split()) > 1:
       admin = await bot.get_chat_member(m.chat.id, m.from_user.id)
       bot_stats = await bot.get_chat_member(m.chat.id, "self")
       user_id = int(m.from_user.id)
