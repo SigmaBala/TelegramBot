4d2900157a72ef66abeefcf262902c05eff9ccef
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-11-29 19:01:13 +0530

    Update broadcast.py

--- MyTgBot/plugins/broadcast.py
+++ MyTgBot/plugins/broadcast.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message
 from pyrogram import filters, errors, enums
 from MyTgBot import bot
 from pyrogram.errors.exceptions.flood_420 import FloodWait
-from MyTgBot.database.db import add_user, add_group, all_users, all_groups, users, groups, remove_user
+from MyTgBot.database.user_db import add_user, add_group, all_users, all_groups, users, groups, remove_user
 import asyncio
 
 @bot.on_message(filters.command(["stats", "users"], ["/", "!", ".", "?"]))
