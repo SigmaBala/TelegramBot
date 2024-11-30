9217975eaf3501896fc0564d1efbf7306c023e5c
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-11-30 18:17:06 +0530

    Update flood_db.py

--- MyTgBot/database/flood_db.py
+++ MyTgBot/database/flood_db.py
@@ -3,21 +3,21 @@ from MyTgBot import mongodb
 db = mongodb["flood"]
 
 async def is_flood_on(chat_id: int) -> bool:
-    chat = await db.find_one({"chat_id": chat_id})
+    chat = db.find_one({"chat_id": chat_id})
     if not chat:
         return True
     return False
 
 
 async def flood_on(chat_id: int):
-    is_flood = await is_flood_on(chat_id)
+    is_flood = is_flood_on(chat_id)
     if is_flood:
         return
-    return await db.delete_one({"chat_id": chat_id})
+    return db.delete_one({"chat_id": chat_id})
 
 
 async def flood_off(chat_id: int):
-    is_flood = await is_flood_on(chat_id)
+    is_flood = is_flood_on(chat_id)
     if not is_flood:
         return
-    return await db.insert_one({"chat_id": chat_id})
+    return db.insert_one({"chat_id": chat_id})
