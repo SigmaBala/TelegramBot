f80c8237d43b5e03c59f94ab1c2615469668e16a
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-12-06 20:48:08 +0530

    Update help_func.py

--- MyTgBot/help/help_func.py
+++ MyTgBot/help/help_func.py
@@ -1,4 +1,5 @@
 from telegraph import upload_file
+from re import findall
 
 async def grap(path):
      try:
@@ -8,3 +9,10 @@ async def grap(path):
      except:
           return False
      return url
+
+def get_urls_from_text(text: str) -> bool:
+    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
+                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
+                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
+                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
+    return [x[0] for x in findall(regex, str(text))]
