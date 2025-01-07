7eb3e293d2daefa7e656994696ab5f8f8f9d25e5
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2025-01-07 07:51:06 +0530

    Update __main__.py

--- MyTgBot/__main__.py
+++ MyTgBot/__main__.py
@@ -1,10 +1,47 @@
-import asyncio
-from __init__ import bot, log
-from web import keep_alive, web_server
+from MyTgBot import bot, log
 from aiohttp import web
 
+import asyncio, aiohttp, logging, traceback
+
+WEB_URL = "https://barath-n3qo.onrender.com"
+WEB_SLLEP = 3*60
+
+
+routes = web.RouteTableDef()
+
+@routes.get('/', allow_head=True)
+async def hello(request):
+    return web.Response(text="Hello, world!")
+
+
+def web_server():
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+
+
+async def keep_alive():
+    if WEB_URL:
+        while True:
+            await asyncio.sleep(WEB_SLLEP)
+            try:
+                async with aiohttp.ClientSession(
+                    timeout=aiohttp.ClientTimeout(total=10)
+                ) as session:
+                    async with session.get(WEB_URL) as resp:
+                        log.info(
+                            "Pinged {} with response: {}".format(
+                                WEB_URL, resp.status
+                            )
+                        )
+            except asyncio.TimeoutError:
+                log.warning("Couldn't connect to the site URL..!")
+            except Exception:
+                traceback.print_exc()
+                    
+
 BIND_ADDRESS = "0.0.0.0"
-PORT = "8080"
+PORT = 8080
 
 async def start_services():        
         server = web.AppRunner(web_server())
