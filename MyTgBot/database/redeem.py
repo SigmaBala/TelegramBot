730b49852344f22783e655bb8ae2e89df7faa520
MrLuffyChan <imluffychan@gmail.com>
2025-01-12 08:37:35 +0530

    Create redeem.py

---
+++ MyTgBot/database/redeem.py
@@ -0,0 +1,48 @@
+import uuid
+from MyTgBot import mongodb
+
+db = mongodb["redeem"]
+
+
+
+async def add_redeem_to_db(bucks: int): 
+    code = str(uuid.uuid4().hex)
+    string = {"code": code, "bucks": bucks}
+    db.insert_one(string)
+    return code
+
+async def remove_redeem_to_db(code: str):
+      string = {"code": code}
+      find = db.find_one(string)
+      if find:
+          db.delete_one(find)
+          return True 
+      else: 
+         return False
+
+
+async def get_redeem_code():
+      code = [x for x in db.find()]
+      return code
+    
+
+
+async def get_redeem_bucks(code, user_id: int):
+     if db.find_one({"code": code}):
+          xx = db.find_one({"code": code})
+          try:
+             xx["user_ids"]
+          except:
+               filter = {"code": code}
+               update = {"$set": {"user_ids": [100]}}
+               db.update_one(filter, update)
+          vv = db.find_one({"code": code})
+          USER_IDS = vv["user_ids"]
+          if user_id not in USER_IDS:
+               USER_IDS.append(user_id)
+               filter = {"code": code}
+               update = {"$set": {"user_ids": USER_IDS}}
+               db.update_one(filter, update)
+               bucks = db.find_one({"code": code})
+               return bucks["bucks"]
+          else: return False
