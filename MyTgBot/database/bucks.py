ff863aaca54606b4cc6b0d787fa604fd4b25e255
MrLuffyChan <imluffychan@gmail.com>
2025-01-12 08:24:43 +0530

    Create bucks.py

---
+++ MyTgBot/database/bucks.py
@@ -0,0 +1,18 @@
+from MyTgBot import mongodb
+
+
+db = mongodb["main"]
+
+
+async def get_bucks_from_users(user_id: int):
+     string = {"user_id": user_id}
+     xx = db.find_one(string)
+     mm = int(xx["bucks"])
+     return mm
+
+async def add_bucks_to_db(user_id: int, bucks: int):
+        xx = await get_bucks_from_users(user_id)
+        total_bucks = xx+bucks
+        filter = {"user_id": user_id}
+        update = {"$set": {"bucks": total_bucks}}
+        db.update_one(filter, update)
